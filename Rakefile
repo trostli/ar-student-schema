bcc293e1eeba9ca1eda71a0051bd4a35c245c14c
dbc-apprentice <dbc-apprentice@devbootcamp.com>
2013-09-11 20:11:02 -0700

    first commit - added name and age to Student model

--- Rakefile
+++ Rakefile
@@ -28,6 +28,10 @@ task "db:populate" do
   StudentsImporter.import
 end
 
+task "db:reset" do
+  # I"ll probably write this when I'm annoyed enough by my current workflow :/
+end
+
 desc 'Retrieves the current schema version number'
 task "db:version" do
   puts "Current version: #{ActiveRecord::Migrator.current_version}"
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,14 @@
 require_relative '../../db/config'
+require 'date'
 
 class Student < ActiveRecord::Base
-# implement your Student model here
+
+  def name
+    self.first_name + " " + self.last_name
+  end
+
+  def age
+    now = Date.today
+    now.year - self.birthday.year - ((now.month > self.birthday.month || (now.month == self.birthday.month && now.day >= self.birthday.day)) ? 0 : 1)
+  end
 end
